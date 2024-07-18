001a91b8fead4af65be096dd4f2050491d088c19
Alejandro Avalos <alejandro2008velazquez@gmail.com>
2024-07-17 21:38:37 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -13,4 +13,13 @@ public class Hero extends Character
     private int rupees = 0;
     public int get_rupees() {return rupees;}
     public void set_rupees(int rupees) {this.rupees = rupees;}
+
+
+    // 3.1 - Inventory Encapsulation [LATER]
+
+
+    // 4.1 - Completed Side Quests Encapsulation
+    private int completed_sidequests = 0;
+    public int get_completed_sidequests() {return completed_sidequests;}
+    public void set_completed_sidequests(int completed_sidequests) {this.completed_sidequests = completed_sidequests;}
 }
